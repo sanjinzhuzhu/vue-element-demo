358b0bcf9e1403ec8c848e7cce095f1365c999fe
sanjinzhuzhu <sanjinzhuzhu@aliyun.com>
2022-12-10 17:44:56 +0800

    review article

--- vue-element/mock/article.js
+++ vue-element/mock/article.js
@@ -1,7 +1,7 @@
 const Mock = require('mockjs')
 
 const List = []
-const count = 100
+const count = 10
 
 const baseContent = '<p>I am testing data, I am testing data.</p><p><img src="https://wpimg.wallstcn.com/4c69009c-0fd4-4153-b112-6cb53d1cf943"></p>'
 const image_uri = 'https://wpimg.wallstcn.com/e4558086-631c-425c-9430-56ffb46e70b3'
@@ -22,7 +22,7 @@ for (let i = 0; i < count; i++) {
     display_time: '@datetime',
     comment_disabled: true,
     pageviews: '@integer(300, 5000)',
-    image_uri,
+    image_uri: image_uri,
     platforms: ['a-platform']
   }))
 }
@@ -32,7 +32,7 @@ module.exports = [
     url: '/vue-element-admin/article/list',
     type: 'get',
     response: config => {
-      const { importance, type, title, page = 1, limit = 20, sort } = config.query
+      const { importance, type, title, page = 1, limit = 8, sort } = config.query
 
       let mockList = List.filter(item => {
         if (importance && item.importance !== +importance) return false
