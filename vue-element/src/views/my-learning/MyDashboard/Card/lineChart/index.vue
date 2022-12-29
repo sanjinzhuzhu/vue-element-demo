696babe30fdbdc23a48bffb47aca07f122e59ec5
sanjinzhuzhu <sanjinzhuzhu@aliyun.com>
2022-12-29 23:31:43 +0800

    test

---
+++ vue-element/src/views/my-learning/MyDashboard/Card/lineChart/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="charts" class="charts" />
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  name: '',
+  mounted() {
+    const lineCharts = echarts.init(this.$refs.charts)
+    lineCharts.setOption({
+
+    })
+  }
+}
+</script>
+
+<style>
+.charts{
+  height: 100%;
+  width: 100%;
+}
+</style>
